e573aaaa216ef1d8d89fb9451f588c0c8bc8d002
muhammadzaid270 <muhammadzaid78689@gmail.com>
2026-01-06 00:43:04 +0500

    feat(transformation): add new method to standardize column names

--- src/ecomma/transform/base_transformer.py
+++ src/ecomma/transform/base_transformer.py
@@ -1,5 +1,6 @@
 import logging
 import pandas as pd
+from typing import Tuple
 
 logger = logging.getLogger(__name__)
 
@@ -7,12 +8,25 @@ class BaseTransformer:
     def __init__(self, file_path) -> None:
         self.file_path = file_path
         self.df = self._dataframe()
+        self.df = self._col_names()
 
     def _dataframe(self) -> pd.DataFrame:
-        if self.file_path.suffix == ".jsonl":
-            df = pd.read_json(self.file_path, lines=True)
-        elif self.file_path.suffix == ".csv":
-            df = pd.read_csv(self.file_path)
+        file_path = self.file_path
+        if file_path.suffix == ".jsonl":
+            df = pd.read_json(file_path, lines=True)
+        elif file_path.suffix == ".csv":
+            df = pd.read_csv(file_path)
         else:
-            raise ValueError(f"Unsupported file format: {self.file_path.suffix}")
+            raise ValueError(f"Unsupported file format: {file_path.suffix}")
+        return df
+    
+    def _col_names(self) -> Tuple[pd.DataFrame, list]:
+        df = self.df
+        df.columns = (
+            df.columns
+            .str.strip()
+            .str.title()
+            .replace(r"\s+", "_", regex=True)
+        )
+        # cols = df.columns.tolist()
         return df
