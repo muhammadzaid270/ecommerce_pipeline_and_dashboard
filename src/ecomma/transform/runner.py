c2f0e13e7334837f6f239b205c8d6a37c9381a76
muhammadzaid270 <muhammadzaid78689@gmail.com>
2026-01-06 01:31:38 +0500

    feat(runner): - Add schema mapping dict - remove bugs from load_files function and pass relevant schema to relevant class

--- src/ecomma/transform/runner.py
+++ src/ecomma/transform/runner.py
@@ -1,15 +1,31 @@
 import logging
-from ecomma.settings.config import RAW_DATA, setup_logging
 from ecomma.transform import OrdersTransformer, PromotionsTransformer, MarketingTransformer
-from ecomma.schema import OrdersSchema, PromotionsSchema, MarketingSchema
+from ecomma.schema import OrderSchema, PromoSchema, MarketingSchema
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, Tuple
 import os
 
-logger = logging.getLogger(__name__)
-setup_logging()
+'''
+fix me:
+1) give schemas to transformers (solved)
+2) receiving outputs from transformers
+'''
 
-def search_files(directory: str = RAW_DATA, extension: tuple[str] = (".jsonl", ".csv")) -> Iterator[Path]:
+logger = logging.getLogger(__name__)
+
+TRANSFORMER_MAPPING = {
+    "orders": OrdersTransformer,
+    "marketing": MarketingTransformer,
+    "promotions": PromotionsTransformer,
+}
+
+SCHEMA_MAPPING = {
+    "orders": OrderSchema.get_schema(),
+    "marketing": MarketingSchema.get_schema(),
+    "promotions": PromoSchema.get_schema(),
+}
+
+def search_files(directory: str = RAW_DATA, extension: Tuple[str, str] = (".jsonl", ".csv")) -> Iterator[Path]:
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(extension):
@@ -17,12 +33,6 @@ def search_files(directory: str = RAW_DATA, extension: tuple[str] = (".jsonl", "
 
 def infer_schema(file_path: Path) -> str:
     return file_path.parent.name
-
-TRANSFORMER_MAPPING = {
-    "orders": OrdersTransformer,
-    "marketing": MarketingTransformer,
-    "promotions": PromotionsTransformer,
-}
 
 def map_transformer(file_path: Path) -> object:
     schema = infer_schema(file_path)
@@ -35,12 +45,9 @@ def load_files() -> None:
     for file_path in search_files(RAW_DATA, (".jsonl", ".csv")):
         logger.info(f"Loading file: {file_path}")
         transformer_obj = map_transformer(file_path)
-        transformer_obj.transform() # A critical thing to do later! regarding schema validation
+        schema = SCHEMA_MAPPING[infer_schema(file_path)]
 
-        if transformer_obj == OrdersSchema(file_path):
-            transformer_obj.validate(schema=OrdersSchema)
-            agg_df = transformer_obj.aggregate()
-            total_rev, total_discounts, net_rev = transformer_obj.revenue(agg_df)
-
-        if transformer_obj == MarketingSchema(file_path):
-            transformer_obj.validate(schema=MarketingSchema)
+if __name__ == "__main__":
+    from ecomma.settings.config import RAW_DATA, setup_logging
+    setup_logging()
+    
