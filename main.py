aeb5b957bb25baef5173624f2fecfd26b70777a2
zaid <zaid.devpro@gmail.com>
2026-01-07 22:50:49 +0500

    minor tweak

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
-from src.settings.config import setup_logging
+from ecomma.settings.config import setup_logging
+from ecomma.extract.runner import run_extraction
 
 def main():
     logger = logging.getLogger(__name__)
     setup_logging()
 
+    logger.info("Starting the extraction process.")
 
+    run_extraction()
 
 if __name__ == "__main__":
     main()
